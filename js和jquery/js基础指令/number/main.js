1adc3704965054ca9198ef5865d5c13a4b157c79
yyysuye <ycxdawn@163.com>
2019-01-22 17:07:28 +0800

    js&jquery

---
+++ js和jquery/js基础指令/number/main.js
@@ -0,0 +1,2 @@
+var cost = document.getElementById("cost");
+cost.textContent = "$ " +20;
---
+++ js和jquery/js基础指令/string/main.js
@@ -0,0 +1,5 @@
+var username = document.getElementById("tname");
+var note = document.getElementById("note");
+username.textContent = "ycx";
+note.textContent = "See our upcoming range";
+//使用name存放获取节点失败
